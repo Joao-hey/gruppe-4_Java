e682c4411a93efb3c4b20b7a070bb937921dae33
williamH2003 <william.hockley@ict.csbe.ch>
2020-11-28 16:44:53 +0100

    PersonenAbfrage

---
+++ src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+import javax.swing.*;
+import java.util.Scanner;
+import java.lang.String;
+
+public class Main {
+    static Scanner myObj = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+        PersonenAbfrage();
+
+    }
+
+
+    static void PersonenAbfrage() {
+
+        System.out.println("Wie heisen Sie zum Vorname: ");
+        String vorname;
+        vorname = myObj.nextLine();
+
+        System.out.println("Wie heisen Sie zum Nachname: ");
+        String nachname;
+        nachname = myObj.nextLine();
+
+        System.out.println("Wie alt sind Sie: ");
+        int age;
+        age = myObj.nextInt();
+
+        if (age >= 16) {
+
+            System.out.println("Geben Sie Ihre Addresse an format (Strasse, Nr)");
+            String addresse;
+            addresse = myObj.nextLine();
+
+            System.out.println("Geben Sie Ihr wohnort an format (Wohnort, PLZ)");
+            String wohnort;
+            wohnort = myObj.nextLine();
+
+        } else {
+            System.exit(0);
+
+        }
+    }
+
+}
