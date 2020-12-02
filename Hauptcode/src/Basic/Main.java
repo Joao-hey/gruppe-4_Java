46214b679304440beedc0d36248339db8d7efba1
williamH2003 <william.hockley@ict.csbe.ch>
2020-12-02 16:35:30 +0100

    Haupt code Fertig

--- Hauptcode/src/Basic/Main.java
+++ Hauptcode/src/Basic/Main.java
@@ -5,72 +5,77 @@ import java.util.Scanner;
 public class Main {
     public static Person[] registerdPerson = new Person[10];
     public static int personCount = 0;
+    public static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
         Person newPerson2 = new Person();
         newPerson2.setFirstName("William");
 
-        // static void Hauptmenue() {
-        System.out.println("1. Create a new Account");
-        System.out.println("2. Show all account's");
-        System.out.println("3. Stop the Programm");
+        Hauptmenue();
+    }
+
+    static void Hauptmenue() {
+        System.out.println("Welcome to are Program");
+        System.out.println("----------------------------------");
+        System.out.println("Create a new Account, press 1");
+        System.out.println("Show all account's, press 2");
+        System.out.println("Stop the Program, press 3");
+        System.out.println("---------------------------------");
         int input = scan.nextInt();
-        /*
-        Mein vorschlag:
+
+        if (input == 1) {
+            Abfrage();
+        }
 
         if (input == 2) {
-            (Code)
-         }
-
-        if (input == 3) {
+            System.out.println("This System is not working at the monent, sorry error M_403");
+        } else if (input == 3) {
             System.exit(0);
-         }
+        }
 
-         */
+    }
 
 
 
-        // static void Abfrage() {
-        while (true)
-        if (input == 1) {
+    static void Abfrage() {
+        while (true) {
             System.out.println("what's your first Name?");
             String firstName = scan.next();
             System.out.println("What's your Last Name?");
             String lastName = scan.next();
-            System.out.println("Is your Name" + firstName + " " + lastName + "?");
+            System.out.println("Is your Name" + "" + firstName + " " + lastName + "?");
             System.out.println("y/n");
             String yes = scan.next();
             if (yes.equals("y")) {
                 Person newPerson = new Person();
                 newPerson.setFirstName(firstName);
                 newPerson.setLastName(lastName);
-                System.out.println("What's your age?");
+                System.out.println("How old are you?");
                 int age = scan.nextInt();
                 if (age >= 16) {
                     newPerson.setAge(age);
-                    System.out.println("Great you are " + age);
-                    System.out.println("On wich street to you live?");
+                    System.out.println("Great you are " + age + "years old.");
+                    System.out.println("On witch street to you live on? (Only Street name)");
                     String street = scan.next();
                     newPerson.setStreet(street);
-                    System.out.println("In wich city?");
+                    System.out.println("In witch city do you live?");
                     String city = scan.next();
                     newPerson.setCity(city);
-                    System.out.println("What's your PLZ");
+                    System.out.println("What's your Postal code (PLZ)");
                     String PLZ = scan.next();
                     System.out.println("");
 
                 }
                 registerdPerson[personCount] = newPerson;
                 personCount++;
-            }
-            else if (yes.equals("n")) {
+            } else if (yes.equals("n")) {
                 System.out.println("Good luck next time");
-            }
-            else {
+            } else {
                 System.out.println("Unable to process Answer");
             }
             break;
         }
+
+        Hauptmenue();
     }
 }
